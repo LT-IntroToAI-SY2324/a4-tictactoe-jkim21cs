d332b6fd572e9c1c6d0880cf52b787e168820a01
Jane Kim <jkim21@cps.edu>
2023-10-23 11:31:30 -0500

    worked on notes.py

--- notes.py
+++ notes.py
@@ -2,3 +2,31 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    #function that start with __(double underscore) are not intended to be called directly
+    def __init__(self, fc = "", a = 0, w= 0.0, n = "")-> None: #java version is this. to denote you are talking about the object at hand the -> None means that nothing is being returned. 
+        """Create an instance of the dog class and set attributes"""
+        self.fur_color = fc
+        self.age = a
+        self.weight = w 
+        self.name = n
+        self.fetch_count = 0
+    def __str__(self) -> str:
+        s = "Dog's name is " + self.name + "\n" #"\n" is the equivalent to pressing enter to move to the next line
+        s+= "and their age is " + str(self.age) + "\n"
+        s+= "and their fur color is " + self.fur_color + "\n"
+        return s
+    def play_fetch(self, num_times):
+        self.fetch_count += num_times
+     
+bergdog = Dog("black", 7, 78.2, "Logan")
+ninadog = Dog("brown", 3, 100, "Hobbes")
+
+print(bergdog)
+print(ninadog)
+
+bergdog.play_fetch(20)
+ninadog.play_fetch(15)
+
+print(f"{ninadog.name} has played fetch {ninadog.fetch_count} times")
+
